eff9ee5c3a52ddb3558aef5616b0765b2c4ed90c
kwiruu <keiru277@gmail.com>
2024-04-09 14:00:33 +0800

    edited the program id from beta.solpg.io

--- constants/index.js
+++ constants/index.js
@@ -1,3 +1,3 @@
 import { PublicKey } from "@solana/web3.js";
 
-export const PROGRAM_PUBKEY = new PublicKey("7DmUqxJv9FkMAGji6FBLRKRvgNTg4EQFU34wyejFMvSB");
+export const PROGRAM_PUBKEY = new PublicKey("9mzyg4oisoWK4bzU32BEPFfM6kMqF3vUTWNjtfkAZ4H5");
