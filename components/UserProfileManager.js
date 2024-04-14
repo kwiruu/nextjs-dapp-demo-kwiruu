859a4113d0d3556bd8127041d8ab1f5d5b3fe610
kwiruu <keiru277@gmail.com>
2024-04-14 23:22:28 +0800

    SBC prototype1 : no autogenerate

--- components/UserProfileManager.js
+++ components/UserProfileManager.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useUserProfile } from '../hooks/demo';
+import Image from 'next/image';
 
 export function UserProfileManager() {
     const { loading, transactionPending, userProfile, initializeUser, editUserProfile, closeUserProfile } = useUserProfile();
@@ -13,26 +14,28 @@ export function UserProfileManager() {
     
 
     if (loading) return <div>Loading...</div>;
+    
 
     // console.log("first name: ", userProfile.firstName)
     return (
-        
-        <div class="shadow-box" style={{ width: '100%', margin: 'auto'}}>
+        <div style={{ width: '100%', margin: 'auto'}}>
             <div style={{display:'flex',justifyContent:'space-between',borderBottom:'2px solid #512b08', width: '100%', margin: 'auto',backgroundColor:'#78bea2',borderTopLeftRadius:'5px',borderTopRightRadius:'5px'}}>
-                <h3 style={{padding:'0px',marginLeft:'20px',color:'#502909',fontFamily:'Roboto', fontWeight:'600'}}>Information</h3>
+                <h3 style={{padding:'0px',marginLeft:'20px',color:'#502909',fontFamily:'Roboto', fontWeight:'600'}}><div style={{display:'flex',justifyContent:'space-between'}}><div style={{height:'15px',width:'15px',borderRadius:'10px',backgroundColor:'#e86e49',margin:'auto',marginLeft:'5px',border:'2px solid #512b08'}}></div><div style={{height:'15px',width:'15px',borderRadius:'10px',backgroundColor:'#ffb703',margin:'auto',marginLeft:'5px',border:'2px solid #512b08'}}></div><div style={{height:'15px',width:'15px',borderRadius:'10px',backgroundColor:'#6fd555',margin:'auto',marginLeft:'5px',border:'2px solid #512b08'}}></div></div></h3>
                 
-                <button onClick={initializeUser} disabled={transactionPending || userProfile} style={{ margin:'10px',marginRight: '20px', backgroundColor:'#78bea2',padding:'10px',border:'2px solid #532d08',borderRadius:'5px',color:'#502909',fontFamily:'Roboto', fontWeight:'600'}}>
+                <button class="window-box-button" onClick={initializeUser} disabled={transactionPending || userProfile} style={{ margin:'10px',marginRight: '20px', backgroundColor:'#78bea2',padding:'10px',border:'2px solid #532d08',borderRadius:'5px',color:'#502909',fontFamily:'Roboto', fontWeight:'600'}}>
                     Initialize Profile
                 </button>
-                </div>
+            </div>
                 <div style={{maxWidth: '500px', margin: 'auto',marginTop:'50px'}}>
             
             {userProfile && (
-                <div style={{ padding: '10px',marginBottom: '50px',color:'#502909',fontFamily:'Roboto', fontWeight:'600', border:'2px solid #532d08', borderRadius:'5px'}}>
-                    <h3>Profile Details:</h3>
-                    <p>First Name: {userProfile.firstName}</p>
-                    <p>Last Name: {userProfile.lastName}</p>
-                    <p>Age: {userProfile.age}</p>
+                <div style={{ padding: '10px',color:'#502909',fontFamily:'Roboto', fontWeight:'600', border:'2px solid #532d08', borderRadius:'5px',display:'flex',justifyContent: 'space-between', width:'80%', margin:'auto',marginBottom: '50px'}}>
+                    <div syle={{width:'100%'}}>
+                        <h3>Profile Details:</h3>
+                        <p>First Name: {userProfile.firstName}</p>
+                        <p>Last Name: {userProfile.lastName}</p>
+                        <p>Age: {userProfile.age}</p>
+                    </div>
                 </div>
             )}
             <div>
@@ -43,32 +46,32 @@ export function UserProfileManager() {
                         value={firstName}
                         onChange={(e) => setFirstName(e.target.value)}
                         placeholder="First Name"
-                        style={{ padding: '8px', marginBottom: '10px' ,backgroundColor:'#f4cecb',border:'2px solid #502909',borderRadius: '5px', color:'#502909',fontFamily:'Roboto', fontWeight:'600'}}
+                        style={{ padding: '8px', marginBottom: '10px' ,backgroundColor:'#f4cecb',border:'2px solid #502909',borderRadius: '5px', color:'#502909',fontFamily:'Roboto', fontWeight:'600',width:'80%',margin:'auto'}}
                     />
                     <input
                         type="text"
                         value={lastName}
                         onChange={(e) => setLastName(e.target.value)}
                         placeholder="Last Name"
-                        style={{ padding: '8px', marginBottom: '10px' ,backgroundColor:'#f4cecb',border:'2px solid #502909',borderRadius: '5px', color:'#502909',fontFamily:'Roboto', fontWeight:'600'}}
+                        style={{ padding: '8px', marginBottom: '10px' ,backgroundColor:'#f4cecb',border:'2px solid #502909',borderRadius: '5px', color:'#502909',fontFamily:'Roboto', fontWeight:'600',width:'80%',margin:'auto'}}
                     />
                     <input
                         type="number"
                         value={age}
                         onChange={(e) => setAge(e.target.value)}
                         placeholder="Age"
-                        style={{ padding: '8px', marginBottom: '10px' ,backgroundColor:'#f4cecb',border:'2px solid #502909',borderRadius: '5px', color:'#502909',fontFamily:'Roboto', fontWeight:'600'}}
+                        style={{ padding: '8px', marginBottom: '10px' ,backgroundColor:'#f4cecb',border:'2px solid #502909',borderRadius: '5px', color:'#502909',fontFamily:'Roboto', fontWeight:'600',width:'80%',margin:'auto'}}
                     />
-                    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center',marginBottom:'8%'}}>
-                    <button onClick={handleEdit} disabled={transactionPending || !userProfile} style={{padding:'10px', paddingLeft:'16%', paddingRight:'16%', marginBottom: '10px',backgroundColor:'#78bea2', border:'2px solid #502909',borderRadius: '5px', color:'#502909',fontFamily:'Roboto', fontWeight:'600'}}>
+                    <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center',marginBottom:'8%'}}>
+                    <button class="window-box-button" onClick={handleEdit} disabled={transactionPending || !userProfile} style={{backgroundColor:'#78bea2', border:'2px solid #502909',borderRadius: '5px', color:'#502909',fontFamily:'Roboto', fontWeight:'600',width:'30%',padding:'10px',marginLeft:'12%',marginTop:'5%'}}>
                         Edit Profile
                     </button>
-                    <button onClick={closeUserProfile} disabled={transactionPending || !userProfile} style={{padding:'10px', paddingLeft:'16%', paddingRight:'16%', marginLeft:'6%', marginBottom: '10px',backgroundColor:'#78bea2', border:'2px solid #502909',borderRadius: '5px', color:'#502909',fontFamily:'Roboto', fontWeight:'600'}}>
+                    <button class="window-box-button" onClick={closeUserProfile} disabled={transactionPending || !userProfile} style={{backgroundColor:'#78bea2', border:'2px solid #502909',borderRadius: '5px', color:'#502909',fontFamily:'Roboto', fontWeight:'600',width:'30%',padding:'10px',marginRight:'12%',marginTop:'5%'}}>
                         Close Profile
                     </button>
                     </div>
                 
-                {!userProfile && <p style={{paddingBottom:'0px',color:'#502909',fontFamily:'Roboto', fontWeight:'600', textAlign:'center'}}>No profile found. Use the button above to initialize a new profile.</p>}
+                {!userProfile && <p style={{paddingBottom:'0px',color:'#502909',fontFamily:'Roboto', fontWeight:'600', textAlign:'center',padding:'5%'}}>No profile found. Use the button above to initialize a new profile.</p>}
                 </div>
             </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,161 @@
+import React, { useState } from 'react';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import { UserProfileManager } from '../components/UserProfileManager';
-import React from 'react';
+import Image from 'next/image';
+import catColorz from '../public/images/color_atlas.gif';
+import catItemz from '../public/images/item_atlas.gif';
+import catBackgroundz from '../public/images/bg_atlas.gif';
+import background_page from '../public/images/Pattern.png';
+
 
 export default function Home() {
-  return (
-    <div style={{backgroundColor: '#df9af7'}}>
-      <h1 style={{textAlign:'center', color:'#502909'}}>SBC Profile Manager</h1>
-        <WalletMultiButton style={{marginLeft: '50px'}}/>
-          <div style={{border:'2px solid #512b08',width:'40%', margin:'auto',borderRadius:'5px',backgroundColor:'#fff5dc'}}>
-          <UserProfileManager/>
+    const [clipPathValueColor, setclipPathValueColor] = useState(0); // Initial clip-path value
+    const [transformValueColor, settransformValueColor] = useState(0); // Initial transform value
+
+    const [clipPathValueEyes, setclipPathValueEyes] = useState(0); // Initial clip-path value
+    const [transformValueEyes, settransformValueEyes] = useState(0); // Initial transform value
+
+    const [clipPathValueBackground, setclipPathValueBackground] = useState(0); // Initial clip-path value
+    const [transformValueBackground, settransformValueBackground] = useState(0); // Initial transform value
+
+    const handlePreviousClickColor = () => {
+    if (transformValueColor < 0) { // Check if transform value is less than 0
+        setclipPathValueColor(clipPathValueColor + 10); // Decrement clip-path value by 10%
+        settransformValueColor(transformValueColor + 10); // Increment transform value by 10%
+    }
+  };
+
+const handleNextClickColor = () => {
+    if (transformValueEyes > -90) {
+        settransformValueColor(transformValueColor - 10);
+        setclipPathValueColor(clipPathValueColor - 10);
+    }
+  };
+
+  const handlePreviousClickEyes = () => {
+    if (transformValueEyes < 0) { // Check if transform value is less than 0
+        setclipPathValueEyes(clipPathValueEyes + 10); // Decrement clip-path value by 10%
+        settransformValueEyes(transformValueEyes + 10); // Increment transform value by 10%
+    }
+  };
+
+const handleNextClickEyes = () => {
+    if (transformValueEyes > -90) {
+        settransformValueEyes(transformValueEyes - 10);
+        setclipPathValueEyes(clipPathValueEyes - 10);
+    }
+  };
+
+  const handlePreviousClickBackground = () => {
+    if (transformValueBackground < 0) { // Check if transform value is less than 0
+        setclipPathValueBackground(clipPathValueBackground + 10); // Decrement clip-path value by 10%
+        settransformValueBackground(transformValueBackground + 10); // Increment transform value by 10%
+    }
+  };
+
+const handleNextClickBackground = () => {
+    if (transformValueBackground > -90) {
+        settransformValueBackground(transformValueBackground - 10);
+        setclipPathValueBackground(clipPathValueBackground - 10);
+    }
+  };
+
+    return (
+        <div>
+          <Image src={background_page} alt="carcolor img!" style={{position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            zIndex: -1,  // Ensure the background is behind other content
+            backgroundImage: `url(${background_page})`,
+            backgroundSize: 'cover',  // Cover the entire background
+            backgroundPosition: 'center',  // Center the background
+            backgroundRepeat: 'no-repeat'
+            }} />
+            <h2 style={{marginLeft:'5%',marginBottom:'3%',border:'2px solid #512b08',textAlign:'center', color:'#502909',width:'30%',backgroundColor:'#f4cecb',borderRadius:'5px'}}><h3 style={{padding:'5px',color:'#502909',fontFamily:'Roboto', fontWeight:'600',borderBottom:'2px solid #512b08',backgroundColor:'#fcd26c', margin:'0px',borderTopLeftRadius:'5px',borderTopRightRadius:'5px',display:'flex',justifyContent:'space-between'}}><div style={{display:'flex'}}><div style={{height:'15px',width:'15px',borderRadius:'10px',backgroundColor:'#e86e49',margin:'auto',marginLeft:'5px',border:'2px solid #512b08'}}></div><div style={{height:'15px',width:'15px',borderRadius:'10px',backgroundColor:'#ffb703',margin:'auto',marginLeft:'5px',border:'2px solid #512b08'}}></div><div style={{height:'15px',width:'15px',borderRadius:'10px',backgroundColor:'#6fd555',margin:'auto',marginLeft:'5px',border:'2px solid #512b08'}}></div></div></h3>SBC Cat Builder</h2>
+            
+            <div style={{display:'flex'}}>
+                <div style={{display:'flex', flexDirection:'column', width:'20%',marginLeft:'5%'}}>
+                    <div class="window-box" style={{border:'2px solid #512b08', marginLeft:'5%',borderRadius:'5px',backgroundColor:'#fff5dc',height:'320px',display:'flex',flexDirection:'column'}}>
+                        <h3 style={{padding:'15px',color:'#502909',fontFamily:'Roboto', fontWeight:'600',borderBottom:'2px solid #512b08',backgroundColor:'#fa8878', margin:'0px',borderTopLeftRadius:'5px',borderTopRightRadius:'5px',display:'flex',justifyContent:'space-between'}}>About me<div style={{display:'flex'}}><div style={{height:'15px',width:'15px',borderRadius:'10px',backgroundColor:'#e86e49',margin:'auto',marginLeft:'5px',border:'2px solid #512b08'}}></div><div style={{height:'15px',width:'15px',borderRadius:'10px',backgroundColor:'#ffb703',margin:'auto',marginLeft:'5px',border:'2px solid #512b08'}}></div><div style={{height:'15px',width:'15px',borderRadius:'10px',backgroundColor:'#6fd555',margin:'auto',marginLeft:'5px',border:'2px solid #512b08'}}></div></div></h3>
+                        <div>
+                                <p style={{padding:'20px',color:'#512b08',fontFamily:'Roboto', fontWeight:'400', fontSize:'0.8em', paddingBottom:'0px'}}>
+                                Hi! I'm Keiru. I am currently studying how to implement this on the Solana blockchain. I'm having problems figuring out how to make this Cat Maker auto-generated using only your name and last name. I plan to use it as a hashtable, converting your information into integers and applying it to the cat maker to generate a cat using your name as a key.</p>
+                                <p style={{padding:'20px',paddingBottom:'10px',color:'#512b08',fontFamily:'Roboto', fontWeight:'400', fontSize:'0.8em'}}>
+                                Big thanks to Pancho in ArtStation as for his pop cat sprite pixel art animation </p>
+                        </div>
+                    </div>
+
+                    <div class="window-box" style={{marginTop:'10%',border:'2px solid #512b08', marginLeft:'5%',borderRadius:'5px',backgroundColor:'#fff5dc',height:'150px',display:'flex',flexDirection:'column'}}>
+                    <h3 style={{padding:'15px',color:'#502909',fontFamily:'Roboto', fontWeight:'600',borderBottom:'2px solid #512b08',backgroundColor:'#78bea2', margin:'0px',borderTopLeftRadius:'5px',borderTopRightRadius:'5px',display:'flex',justifyContent:'space-between'}}>Contact me<div style={{display:'flex'}}><div style={{height:'15px',width:'15px',borderRadius:'10px',backgroundColor:'#e86e49',margin:'auto',marginLeft:'5px',border:'2px solid #512b08'}}></div><div style={{height:'15px',width:'15px',borderRadius:'10px',backgroundColor:'#ffb703',margin:'auto',marginLeft:'5px',border:'2px solid #512b08'}}></div><div style={{height:'15px',width:'15px',borderRadius:'10px',backgroundColor:'#6fd555',margin:'auto',marginLeft:'5px',border:'2px solid #512b08'}}></div></div></h3>
+                        <div>
+                            <li>
+                                <ul style={{color:'#502909',fontFamily:'Roboto', fontWeight:'600',fontSize:'0.8em',margin:'2px'}}>Discord  : watadamz_277</ul>
+                                <ul style={{color:'#502909',fontFamily:'Roboto', fontWeight:'600',fontSize:'0.8em',margin:'2px'}}> Gmail    : keiruvent.cabili@gmail.com</ul>
+                                <ul style={{color:'#502909',fontFamily:'Roboto', fontWeight:'600',fontSize:'0.8em',margin:'2px'}}>LinkedIn : keiru-cabili-6944552a4</ul>
+                            </li>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="window-box" style={{border:'2px solid #512b08',width:'25%', marginLeft:'5%',marginRight:'10%',borderRadius:'5px',backgroundColor:'#fff5dc',height:'80%'}}>
+                    <UserProfileManager/>
+                </div>
+                <div style={{display:'flex', flexDirection:'column',width:'20%'}}>
+                        
+                    <div class="window-box" style={{height:'430px',border:'2px solid #512b08',marginRight:'10%',borderRadius:'5px',backgroundColor:'#fff5dc'}}>
+                        <h3 style={{padding:'15px',color:'#502909',fontFamily:'Roboto', fontWeight:'600',borderBottom:'2px solid #512b08',backgroundColor:'#fa8878', margin:'0px',borderTopLeftRadius:'5px',borderTopRightRadius:'5px',display:'flex',justifyContent:'space-between'}}>Cat Maker<div style={{display:'flex'}}><div style={{height:'15px',width:'15px',borderRadius:'10px',backgroundColor:'#e86e49',margin:'auto',marginLeft:'5px',border:'2px solid #512b08'}}></div><div style={{height:'15px',width:'15px',borderRadius:'10px',backgroundColor:'#ffb703',margin:'auto',marginLeft:'5px',border:'2px solid #512b08'}}></div><div style={{height:'15px',width:'15px',borderRadius:'10px',backgroundColor:'#6fd555',margin:'auto',marginLeft:'5px',border:'2px solid #512b08'}}></div></div></h3>
+
+                        <div class="container-avatar">
+                            <div class="avatar fit">
+                            
+                                <div className="image-container" style={{overflow: 'hidden'}}>
+                                <Image src={catColorz} class="color-overlay" alt="carcolor img!" style={{
+                                    clipPath: `inset(0% ${clipPathValueColor+90}% 90% ${-clipPathValueColor}%)`, // Adjusted clip-path value
+                                    transform: `translate(${transformValueColor}%, 0%)`,position:'absolute' // Adjusted transform value
+                                }} unoptimized />
+
+                                <Image src={catItemz} class="color-overlay" alt="carcolor img!" style={{
+                                    clipPath: `inset(0% ${clipPathValueEyes+90}% 90% ${-clipPathValueEyes}%)`, // Adjusted clip-path value
+                                    transform: `translate(${transformValueEyes}%, 0%)`,position:'absolute' // Adjusted transform value
+                                }} unoptimized/>
+
+                                <Image src={catBackgroundz} class="color-overlay-bg" alt="carcolor img!" style={{
+                                    clipPath: `inset(0% ${clipPathValueBackground+90}% 90% ${-clipPathValueBackground}%)`, // Adjusted clip-path value
+                                    transform: `translate(${transformValueBackground}%, 0%)`,position:'absolute' // Adjusted transform value
+                                }}/> 
+                                </div>
+
+                            </div>
+                            <div style={{display:'flex',justifyContent:'space-between',paddingTop:'20px'}}>
+                                <div class="button-maker" style={{display:'flex', flexDirection:'column'}}>
+                                    <button class="window-box-button" onClick={handlePreviousClickColor} style={{backgroundColor:'#fa8878', border:'2px solid #502909',borderRadius: '5px', color:'#502909',fontFamily:'Roboto', fontWeight:'600'}}>◄</button>
+                                    <button class="window-box-button" onClick={handlePreviousClickEyes} style={{backgroundColor:'#fa8878', border:'2px solid #502909',borderRadius: '5px', color:'#502909',fontFamily:'Roboto', fontWeight:'600'}}>◄</button>
+                                    <button class="window-box-button" onClick={handlePreviousClickBackground} style={{backgroundColor:'#fa8878', border:'2px solid #502909',borderRadius: '5px', color:'#502909',fontFamily:'Roboto', fontWeight:'600'}}>◄</button>
+                                </div>
+                                <div style={{display:'flex', flexDirection:'column', color:'#502909',fontFamily:'Roboto', fontWeight:'600',fontSize:'0.7em',textAlign:'center'}}>
+                                    <p>Color</p>
+                                    <p>Items</p>
+                                    <p>Background</p>
+                                </div>
+                                <div class="button-maker" style={{display:'flex', flexDirection:'column'}}>
+                                    <button class="window-box-button" onClick={handleNextClickColor} style={{backgroundColor:'#fa8878', border:'2px solid #502909',borderRadius: '5px', color:'#502909',fontFamily:'Roboto', fontWeight:'600'}}>►</button>
+                                    <button class="window-box-button" onClick={handleNextClickEyes} style={{backgroundColor:'#fa8878', border:'2px solid #502909',borderRadius: '5px', color:'#502909',fontFamily:'Roboto', fontWeight:'600'}}>►</button>
+                                    <button class="window-box-button" onClick={handleNextClickBackground} style={{backgroundColor:'#fa8878', border:'2px solid #502909',borderRadius: '5px', color:'#502909',fontFamily:'Roboto', fontWeight:'600'}}>►</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    
+                    <div class="window-box" style={{marginTop:'10%',width:'90%',border:'2px solid #502909',borderRadius: '5px', color:'#502909',fontFamily:'Roboto', fontWeight:'600',backgroundColor:'#fff5dc'}}>
+                    <h3 style={{padding:'5px',color:'#502909',fontFamily:'Roboto', fontWeight:'600',borderBottom:'2px solid #512b08',backgroundColor:'#fcd26c', margin:'0px',borderTopLeftRadius:'5px',borderTopRightRadius:'5px',display:'flex',justifyContent:'space-between'}}><div style={{display:'flex'}}><div style={{height:'15px',width:'15px',borderRadius:'10px',backgroundColor:'#e86e49',margin:'auto',marginLeft:'5px',border:'2px solid #512b08'}}></div><div style={{height:'15px',width:'15px',borderRadius:'10px',backgroundColor:'#ffb703',margin:'auto',marginLeft:'5px',border:'2px solid #512b08'}}></div><div style={{height:'15px',width:'15px',borderRadius:'10px',backgroundColor:'#6fd555',margin:'auto',marginLeft:'5px',border:'2px solid #512b08'}}></div></div></h3>
+                        <WalletMultiButton  style={{margin:'15%',width:'100%',backgroundColor:'#fcd26c',border:'2px solid #502909',color:'#502909',fontFamily:'Roboto', fontWeight:'600'}}/>
+                    </div>
+
+                </div>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
